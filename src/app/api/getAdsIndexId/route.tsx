65be757e943d53ff90c78a5a334fd18a9fc98b61
MKim <mrn.kim07@gmail.com>
2024-10-22 17:30:08 +0900

    Remove get index id routes and use NEXT_PUBLIC instead

--- src/app/api/getAdsIndexId/route.tsx
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const adsIndexId = process.env.TWELVELABS_ADS_INDEX_ID;
-  return NextResponse.json({ adsIndexId });
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,47 @@
 "use client";
 
 import { useState } from 'react';
-import { useQuery } from "@tanstack/react-query";
 import Footage from './Footage';
 import Ads from './Ads';
-import { fetchFootageIndexId, fetchAdsIndexId } from '@/hooks/apiHooks';
+
+const footageIndexId = process.env.NEXT_PUBLIC_FOOTAGE_INDEX_ID;
+const adsIndexId = process.env.NEXT_PUBLIC_ADS_INDEX_ID;
 
 export default function Page() {
   const [hashtags, setHashtags] = useState<string[]>([]);
-  console.log("🚀 > Page > hashtags=", hashtags)
   const [footageVideoId, setFootageVideoId] = useState<string>('');
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [isRecommendClicked, setIsRecommendClicked] = useState(false);
 
-  const { data: footageIndexId, isLoading: isFootageIndexIdLoading } = useQuery({
-    queryKey: ['footageIndexId'],
-    queryFn: fetchFootageIndexId,
-  });
-
-  const { data: adsIndexId, isLoading: isAdsIndexIdLoading } = useQuery({
-    queryKey: ['adsIndexId'],
-    queryFn: fetchAdsIndexId,
-  });
-
- return (
+  return (
     <main className="flex min-h-screen p-24">
-    <div className="flex w-full max-w-7xl mx-auto">
-      <div className="w-2/3 pr-4">
-      <Footage
-        setHashtags={setHashtags}
-        indexId={footageIndexId?.footageIndexId}
-        isIndexIdLoading={isFootageIndexIdLoading}
-        footageVideoId={footageVideoId}
-        setFootageVideoId={setFootageVideoId}
-        selectedFile={selectedFile}
-        setSelectedFile={setSelectedFile}
-        setIsRecommendClicked={setIsRecommendClicked}
-      />
+      <div className="flex w-full max-w-7xl mx-auto">
+        <div className="w-2/3 pr-4">
+          <Footage
+            setHashtags={setHashtags}
+            indexId={footageIndexId ?? ''}
+            isIndexIdLoading={!footageIndexId}
+            footageVideoId={footageVideoId}
+            setFootageVideoId={setFootageVideoId}
+            selectedFile={selectedFile}
+            setSelectedFile={setSelectedFile}
+            setIsRecommendClicked={setIsRecommendClicked}
+          />
+        </div>
+        <div className="w-1/6"></div>
+        <div className="w-2/3 pl-4">
+          <Ads
+            hashtags={hashtags}
+            setHashtags={setHashtags}
+            indexId={adsIndexId ?? ''}
+            isIndexIdLoading={!adsIndexId}
+            footageVideoId={footageVideoId}
+            selectedFile={selectedFile}
+            isRecommendClicked={isRecommendClicked}
+            setIsRecommendClicked={setIsRecommendClicked}
+          />
+        </div>
       </div>
-      <div className="w-1/6"></div>
-      <div className="w-2/3 pl-4">
-      <Ads
-        hashtags={hashtags}
-        setHashtags={setHashtags}
-        indexId={adsIndexId?.adsIndexId}
-        isIndexIdLoading={isAdsIndexIdLoading}
-        footageVideoId={footageVideoId}
-        selectedFile={selectedFile}
-        isRecommendClicked={isRecommendClicked}
-        setIsRecommendClicked={setIsRecommendClicked}
-      />
-      </div>
-    </div>
-  </main>
+    </main>
   );
 }
