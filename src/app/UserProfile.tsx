5ce2b098565c7c80cd1bc76b70b6b6a98e658530
MKim <mrn.kim07@gmail.com>
2025-01-16 14:41:28 +0900

    Disable Search button when there's no interest keyword

--- src/app/UserProfile.tsx
+++ src/app/UserProfile.tsx
@@ -625,7 +625,7 @@ function UserProfile({
             onClick={() => {
               setIsSearchClicked(true);
             }}
-            disabled={useEmbeddings && processingAdsInPersonalizedContent}
+            disabled={(useEmbeddings && processingAdsInPersonalizedContent) || interests.length === 0}
           >
             <div className="flex items-center">
               {useEmbeddings ? "Search by Embeddings" : "Search"}
